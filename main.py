4ce8b193d4dddfda7ebdad2acded641bd7dfc4ae
L0SG <tkdrlf9202@gmail.com>
2017-03-07 15:47:54 +0900

    first running model, needs opt

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import tensorflow as tf
 import keras
 import keras.preprocessing.text as kt
-from keras.layers import Input, Embedding, LSTM, Dense, merge
+from keras.layers import Input, Embedding, LSTM, MaxoutDense, Dense, Dropout, merge
+from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 import numpy as np
 import os
 import datamodule
 from sklearn.preprocessing import normalize
 from keras.layers.core import Lambda
+from keras.utils.np_utils import to_categorical
+from keras.backend.tensorflow_backend import set_session
+
+# VRAM limit, typical
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+set_session(tf.Session(config=config))
 
 # dimension of input feature
 INPUT_SIZE = 13
@@ -16,7 +24,7 @@ INPUT_SIZE = 13
 idx_discrete = [0, 1, 3, 6, 8, 10]
 idx_continuous = [2, 4, 5, 7, 9, 11, 12]
 
-# batch size is explicitly 1 for stateful LSTM
+# batch size == seq_length is explicitly 1 for stateful LSTM
 batch_size = 1
 
 print('Input feature column size: ' + str(INPUT_SIZE))
@@ -85,7 +93,23 @@ x_embed = merge([input_embed[i] for i in xrange(len(idx_discrete))], mode='conca
 x = merge([x_embed, input_continuous_expanded], mode='concat')
 
 # apply the vectors to the LSTM layers
-y = LSTM(128, batch_input_shape=(batch_size, x.shape[1], x.shape[2]), stateful=True)(x)
+y = LSTM(128, batch_input_shape=(batch_size, x.shape[1], x.shape[2]), stateful=True, return_sequences=True)(x)
+y = BatchNormalization()(y)
+y = Dropout(0.2)(y)
+y = LSTM(128, stateful=True)(y)
+y = BatchNormalization()(y)
+y = Dropout(0.2)(y)
+
+# some more layers
+y = MaxoutDense(512)(y)
+y = BatchNormalization()(y)
+y = Dropout(0.5)(y)
+y = MaxoutDense(512)(y)
+y = BatchNormalization()(y)
+y = Dropout(0.5)(y)
+y = MaxoutDense(512)(y)
+y = BatchNormalization()(y)
+y = Dropout(0.5)(y)
 
 # discrete output vector generation
 output_discrete = [None for i in xrange(len(idx_discrete))]
@@ -101,13 +125,52 @@ output_continuous = Dense(output_dim=len(idx_continuous),
 model = Model(input=input_discrete+[input_continuous],
               output=output_discrete+[output_continuous])
 
-model.compile(optimizer='adam', loss='categorical_crossentropy')
+loss_dict = {'output_discrete'+str(i): 'kld' for i in xrange(len(idx_discrete))}
+loss_dict.update({'output_continuous': 'kld'})
 
+model.compile(optimizer='adam',
+              loss=loss_dict)
+model.summary()
+
+####################### model construction end, modulate for cleaner code?
+
+####################### input data formulation
+
+# formulate x and y for model.fit, corresponding to the defined input above
+# should split the data to (# of discrete feature) + 1 continuous feature array = # + 1
+# example: 5 discrete, 8 continuous => split to 5 + 1 = 6 subarrays
+feed_x_discrete = [data_train_tokenized[:-1, i] for i in xrange(len(idx_discrete))]
+feed_x_continuous = [data_train_continuous[:-1, :]]
+"""
+# reshape to (nb_samples, seq_length=1, nb_features)
+for i in xrange(len(idx_discrete)):
+    feed_x_discrete[i] = np.reshape(feed_x_discrete[i], (feed_x_discrete[i].shape[0], 1))
+# continuous feed has just one element of ndarray
+# damn it's getting ugly, better ways?
+feed_x_continuous[0] = np.reshape(feed_x_continuous[0], (feed_x_continuous[0].shape[0], feed_x_continuous[0].shape[1]))
+"""
+# merge
+feed_x = feed_x_discrete + feed_x_continuous
+
+# same goes to y, but should convert to one hot encoding for discrete features
+feed_y_discrete = [data_train_tokenized[1:, i] for i in xrange(len(idx_discrete))]
+feed_y_continuous = [data_train_continuous[1:, :]]
+for i in xrange(len(idx_discrete)):
+    # vocabulary is one-based, and to_categorical is zero-based (fuck)
+    feed_y_discrete[i] = to_categorical(np.add(feed_y_discrete[i], -1), nb_classes=len(tokenizers[i].word_index))
+    #feed_y_discrete[i] = np.reshape(feed_y_discrete[i], (feed_y_discrete[i].shape[0], feed_y_discrete[i].shape[1]))
+#feed_y_continuous[0] = np.reshape(feed_y_continuous[0], (feed_y_continuous[0].shape[0], feed_y_continuous[0].shape[1]))
+feed_y = feed_y_discrete + feed_y_continuous
+
+# softmax from discrete output is zero-based
+# tokenizer is one-based
+# be cautious when inference
 for i in xrange(1000):
-    model.fit(x=data_train_tokenized[:-1, :]+data_train_continuous[:-1, :],
-              y=data_train_tokenized[1:, :]+data_train_continuous[1:, :],
+    model.fit(x=feed_x,
+              y=feed_y,
               batch_size=batch_size,
               nb_epoch=1,
-              verbose=2,
+              verbose=1,
               shuffle=False)
     model.reset_states()
+
